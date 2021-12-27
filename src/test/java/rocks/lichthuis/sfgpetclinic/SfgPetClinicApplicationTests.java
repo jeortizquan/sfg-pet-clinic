b5187875eda61d85ccb43215e0052628481ea35d
Jorge Ortiz <jeortiz.quan@gmail.com>
2021-12-27 03:18:43 +0100

    Initial Project Add

---
+++ src/test/java/rocks/lichthuis/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.lichthuis.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
